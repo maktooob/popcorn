06911f244dde3b4435cb96f0d5962c2fef7fdb72
Tobias Conrad <max.tobias.conrad@gmail.com>
2022-07-26 17:19:44 +0200

    add delete function

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,23 +1,57 @@
+import { useState } from "react";
 import moviesArray from "../data/movies.json"
-import Movie from "./Movie";
+import "./Main.css"
 
 function Main() {
 
+    const [movies, setMovies] = useState(moviesArray);
 
-  return (
-    <div>
-      {moviesArray.map((element) => {
-        return (
-          <Movie 
-                  title={element.title} 
-                  rating={element.rating} 
-                  id={element.id} year={element.year} 
-                  image={element.imgURL}
-                  />
-        );
-      })}
-    </div>
-  );
+    const deleteMovie = (movieId) => {
+        setMovies( (prevMovies) => {
+            const newList = prevMovies.filter( (element) => {
+                return element.id !== movieId;
+            });
+            return newList;
+        });
+    }
+
+
+    let message;
+
+    if(movies.length > 0) {
+        message = <h2>There's {movies.length} amount of movies in our DB</h2>;
+    } else {
+        message = <h2>No movies.... 😔</h2>
+    }
+
+
+    return (
+        <div className="Main">
+
+            {message}
+
+            {movies.map((movieObj) => {
+                return (
+                    <div key={movieObj.id} className="movie">
+                        <h2>Title: {movieObj.title}</h2>
+                        <h3>Rating: {movieObj.rating}</h3>
+
+                        {/*
+                        {movieObj.imgURL && <img src={movieObj.imgURL} alt={movieObj.title} />}
+                        */}
+                        
+                        { movieObj.imgURL 
+                        ? <img src={movieObj.imgURL} alt={movieObj.title} /> 
+                        : <p>Sorry, no image</p>
+                        }
+
+                        <button onClick={() => { deleteMovie(movieObj.id) }}>Delete</button>
+                    </div>
+                )
+            })}
+
+        </div>
+    );
 }
 
 export default Main;
